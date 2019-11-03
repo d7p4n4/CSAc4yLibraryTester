6ebc33a24932624486369ee3af5874a5b61fa16b
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-03 10:16:50 +0000

    Add project files.

---
+++ CSAc4yLibraryTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSAc4y.Class;
+
+
+namespace CSAc4yLibraryTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new Ac4yContext())
+            {
+                var a = new Ac4y();
+
+                ctx.Ac4ys.Add(a);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
